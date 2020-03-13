95de186a1f8b17273b25f296a2a222fd6ab73878
proghany <proghany@ebay.com>
2020-03-13 14:48:35 +0100

    rename and fix stuff so event creation and sign up will work

--- src/controllers/events-controller.js
+++ src/controllers/events-controller.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const controller = Router();
-const { createEvent, findMostRecentPastEvent } = require("../models/event-model")
+const { createEvent, findMostRecentPastEvent, findNextTwoEvents, signUpUserForEvent, findEventById } = require("../models/event-model")
 
 controller.post("/events", async (req, res) => {
-  console.log("-------CREATING EVENT IN CONTROLLER-------")
+  console.log("-------CREATING EVENT IN CONTROLLER-------", req.body)
 
-  if (!req.body.eventDate) {
+  if (!req.body.event_date) {
     return res.status(400).json({ errorMsg: "Event date is required!" })
   }
 
@@ -14,14 +14,30 @@ controller.post("/events", async (req, res) => {
 
 });
 
+controller.patch("/events/signup", async (req, res) => {
+  console.log("-------SIGNING UP FOR EVENT IN CONTROLLER-------", req.body)
+
+  if (!req.body.userId || !req.body.eventId) {
+    return res.sendStatus(400)
+  }
+  const foundEvent = await findEventById(req.body.eventId);
+  if (foundEvent.participants && foundEvent.participants.includes(req.body.userId)) {
+    return res.status(200).json({msg: "User is already signed up!"})
+  }
+  const newParticipant = await signUpUserForEvent(req.body.userId, req.body.eventId);
+  console.log("NEW PARTICIPANTS", newParticipant);
+  res.status(201).json(newParticipant)
+
+});
+
 controller.get("/events", async (req, res) => {
   console.log("-------GETTING EVENT IN CONTROLLER-------")
 
-  if (req.query.filter === "past") {
-
-    const pastEvent = await findMostRecentPastEvent();
-    return res.json(pastEvent)
-  }
+  const pastEvent = await findMostRecentPastEvent();
+  const nextEvents = await findNextTwoEvents();
+  const eventsData = {pastEvent, nextEvents};
+  console.log(eventsData)
+  return res.status(200).json(eventsData)
 });
 
 module.exports = controller;
--- src/models/event-model.js
+++ src/models/event-model.js
@@ -1,4 +1,4 @@
-module.exports = {
+const Event = module.exports = {
 
   ensureTable() {
     console.log("EVENTS TABLE")
@@ -10,7 +10,7 @@ module.exports = {
             number_of_fields INTEGER,
             location         VARCHAR(250),
             creator_id       INTEGER,
-            participants     integer[]
+            participants     INTEGER ARRAY
         );
     `);
   },
@@ -21,13 +21,13 @@ module.exports = {
     `)
   },
 
-  createEvent({ eventDate, numberOfFields, location, userId }) {
+  createEvent({ event_date, number_of_fields, location, creator_id }) {
     console.log("CREATING EVENT IN DB")
     return global.client.query(`
         INSERT into events (event_date, number_of_fields, location, creator_id)
         VALUES ($1, $2, $3, $4)
         RETURNING *
-    `, [eventDate, numberOfFields, location, userId])
+    `, [event_date, number_of_fields, location, creator_id])
       .then(res => {
         return res.rows[0]
       })
@@ -43,6 +43,56 @@ module.exports = {
     `)
       .then(res => {
         return res.rows;
+      })
+  },
+
+  findNextTwoEvents() {
+    return global.client.query(`
+        SELECT *
+        FROM events
+        WHERE event_date > NOW()
+        ORDER BY event_date ASC
+    `)
+      .then(res => {
+        console.log("-------------EVENTS NACH JETZT----------------", res.rows)
+        return res.rows.slice(0, 2);
+      })
+  },
+
+  // uncaughtException: malformed array literal: "28"
+  signUpUserForEvent(userId, eventId) {
+    console.log("SIGNING UP FOR EVENT IN DB", userId, eventId)
+    return Event.findEventById(eventId).then(res => {
+      console.log("RIGHT EVENT?========================", res)
+      if (!res.participants) {
+        res.participants = [userId]
+      } else if (!res.participants.includes(userId)) {
+        res.participants.push(userId);
+      } else {
+        return res;
+      }
+
+      return global.client.query(`
+          UPDATE events
+          SET participants = $1
+          WHERE id = $2
+          RETURNING *
+      `, [res.participants, eventId])
+        .then(res => {
+          return res.rows[0]
+        })
+    })
+
+  },
+
+  findEventById(eventId) {
+    return global.client.query(`
+        SELECT *
+        FROM events
+        WHERE $1 = id
+    `, [eventId])
+      .then(res => {
+        return res.rows[0];
       })
   }
 };
