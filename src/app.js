dead73e60be8b199eb74a2e1e9cee250c9d46e87
proghany <proghany@ebay.com>
2021-05-19 10:06:26 +0200

    logging stuff

--- src/app.js
+++ src/app.js
@@ -9,17 +9,20 @@ const cors = require("cors");
 module.exports = async ({ database = process.env.DATABASE, connectionString } = {}) => {
   app.use(morgan("dev"));
   app.use(bodyParser.json());
-
+  console.log("CONNECTING")
   const dbConfig = connectionString
     ? { connectionString }
     : {
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database,
-      };
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      database,
+    };
+  console.log("dbConfig", dbConfig)
 
   global.client = new Client(dbConfig);
   const oldPoolQuery = global.client.query;
+  console.log("oldPoolQuery", oldPoolQuery)
+
   global.client.query = (...args) => {
     console.log("QUERY:", args);
     return oldPoolQuery.apply(global.client, args);
